82e869ea79fee73a06e839d9a96e2e44da7f1970
pelonidas <janx803y@easv365.dk>
2023-02-21 19:56:40 +0100

    refactoring

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,5 @@ import ContactBanner from "@/components/common/contact-banner";
 import Header from "@/components/common/header";
 
 export default function HomePage() {
-  return (
-    <div>
-      <Header />
-      <ArticleCard
-        category="Travel"
-        title="I Created a Developer Rap Video - Here's What I Learned from it. Check it out"
-        dateAdded="02 december 2022"
-        minutesToRead={3}
-        author="Jesica koli"
-        excerpt="Did you come here for something in particular or just general Riker-bashing? And blowing into maximum warp"
-      />
-      <ContactBanner />
-    </div>
-  );
+  return <div></div>;
 }
