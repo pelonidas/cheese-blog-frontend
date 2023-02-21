6f0ba8cfe75add2c67a9ea1bcf5b51a72f609a80
OliverSoucc <oliv572v@easv365.dk>
2023-02-21 19:57:30 +0100

    footer

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,3 +1,44 @@
-export default function Button() {
-  return <div>Button</div>;
+import {HTMLAttributeAnchorTarget} from "react";
+import Link from 'next/link';
+export interface IButtonProps {
+    variant:
+        | 'unstyled'
+        | 'newsletter';
+    className?: string;
+    href?: string;
+    children: React.ReactNode;
+    height?: string;
+    onClick?: () => void;
+    target?: HTMLAttributeAnchorTarget;
+}
+
+export default function Button({
+                                   variant,
+                                   href,
+                                   className,
+                                   children,
+                                   height = 'h-[45px]',
+                                   onClick,
+                                   target,
+                               }: IButtonProps) {
+    const getClass = () => {
+        switch (variant) {
+            case 'unstyled':
+                return '';
+            case 'newsletter':
+                return 'px-[40px] py-[15px] text-white bg-primary text-[15px] leading-[15px]';
+        }
+    };
+
+  return (
+      <Link href={href || '/'} target={target} scroll={false}>
+          <button
+              onClick={onClick}
+              style={{  }}
+              className={`${className} ${height}  ${getClass()} `}
+          >
+              {children}
+          </button>
+      </Link>
+  );
 }
---
+++ components/ui/newsletter-input.tsx
@@ -0,0 +1,16 @@
+import Button from "@/components/ui/button";
+
+
+export default function NewsletterInput() {
+    return (
+        <>
+            <div className={'mb-[40px]'}>
+                <h6 className={'text-black mb-[20px]'}>Subscribe for newsletter</h6>
+                <div className={'flex rounded-lg overflow-hidden'}>
+                    <input className={'h-[45px]'} type="text" name="email" id="email"/>
+                    <Button variant={'newsletter'}>Subcribe</Button>
+                </div>
+            </div>
+        </>
+    )
+}
