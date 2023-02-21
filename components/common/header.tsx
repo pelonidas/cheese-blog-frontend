40eca8278185b5a26d27fd7b2b6ab92b25512f6a
pelonidas <janx803y@easv365.dk>
2023-02-21 19:22:02 +0100

    logo pushed

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,12 +1,17 @@
+import Logo from "@/components/icons/logo";
+import Image from "next/image";
+import cheeseLogo from "/cheese.png";
 export default function Header() {
   return (
     <nav className={"h-[70px] bg-primary/30 flex items-center text-secondary"}>
-      <div className={"container"}>
+      <div className={"container flex items-center"}>
         <ul className={"flex gap-10 font-medium"}>
           <li>Home</li>
           <li>About</li>
           <li>Categories</li>
         </ul>
+
+        <Logo />
       </div>
     </nav>
   );
