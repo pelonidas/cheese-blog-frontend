ecd10d5723233e8a865ab94ab347d0106e1d5c7a
pelonidas <janx803y@easv365.dk>
2023-02-21 20:10:27 +0100

    Heading component created

---
+++ components/ui/heading.tsx
@@ -0,0 +1,14 @@
+interface HeadingProps {
+  label: string;
+}
+
+export default function Heading({ label }: HeadingProps) {
+  const firstWord = label.split(" ")[0];
+  const rest = label.split(" ").slice(1).join(" ");
+  return (
+    <h3 className={"font-semibold text-xl"}>
+      <span className={"bg-primary text-white px-1"}>{firstWord}</span>{" "}
+      <span>{rest}</span>
+    </h3>
+  );
+}
