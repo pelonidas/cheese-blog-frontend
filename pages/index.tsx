c1f138565fc2f49e5a1878454f90afe1b0022b5c
pelonidas <janx803y@easv365.dk>
2023-02-21 20:41:21 +0100

    index posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,68 @@
+import ArticleCard from "@/components/blog/article-card";
+import ContactBanner from "@/components/common/contact-banner";
 import Hero from "@/components/common/hero";
+import Heading from "@/components/ui/heading";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <section className={"container flex gap-20 py-20"}>
+        <div className={"w-full max-w-[855px]"}>
+          <Heading label="recently posted" />
+          <div className={"mt-[55px] flex flex-col gap-12"}>
+            <ArticleCard
+              category="Travel"
+              title="I Created a Developer Rap Video - Here's What I Learned from it. Check it out"
+              dateAdded="02 december 2022"
+              minutesToRead={3}
+              author="Jesica koli"
+              excerpt="Did you come here for something in particular or just general Riker-bashing? And blowing into maximum warp"
+            />
+            <ArticleCard
+              category="Travel"
+              title="I Created a Developer Rap Video - Here's What I Learned from it. Check it out"
+              dateAdded="02 december 2022"
+              minutesToRead={3}
+              author="Jesica koli"
+              excerpt="Did you come here for something in particular or just general Riker-bashing? And blowing into maximum warp"
+            />
+            <ContactBanner />
+            <ArticleCard
+              category="Travel"
+              title="I Created a Developer Rap Video - Here's What I Learned from it. Check it out"
+              dateAdded="02 december 2022"
+              minutesToRead={3}
+              author="Jesica koli"
+              excerpt="Did you come here for something in particular or just general Riker-bashing? And blowing into maximum warp"
+            />
+            <ArticleCard
+              category="Travel"
+              title="I Created a Developer Rap Video - Here's What I Learned from it. Check it out"
+              dateAdded="02 december 2022"
+              minutesToRead={3}
+              author="Jesica koli"
+              excerpt="Did you come here for something in particular or just general Riker-bashing? And blowing into maximum warp"
+            />
+            <ArticleCard
+              category="Travel"
+              title="I Created a Developer Rap Video - Here's What I Learned from it. Check it out"
+              dateAdded="02 december 2022"
+              minutesToRead={3}
+              author="Jesica koli"
+              excerpt="Did you come here for something in particular or just general Riker-bashing? And blowing into maximum warp"
+            />
+            <ArticleCard
+              category="Travel"
+              title="I Created a Developer Rap Video - Here's What I Learned from it. Check it out"
+              dateAdded="02 december 2022"
+              minutesToRead={3}
+              author="Jesica koli"
+              excerpt="Did you come here for something in particular or just general Riker-bashing? And blowing into maximum warp"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
